68672e366a669a1245c69377a1a31d1c6f67db20
rionaldi <rionaldi.dwipurna@gmail.com>
2024-11-21 20:10:23 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 #![allow(unused_imports)]
 use core::panic;
 use core::str;
+use std::collections::HashMap;
+use std::error::Error;
 use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::sync::{Arc, MutexGuard, RwLock};
 use std::thread;
+
+enum RedisResponse {
+    OK(String),
+    Error(String),
+}
+
+impl RedisResponse {
+    fn to_string(&self) -> String {
+        match self {
+            RedisResponse::OK(message) => format!("+{}\r\n", message),
+            RedisResponse::Error(message) => format!("-{}\r\n", message),
+        }
+    }
+}
 
 //#[derive(Debug)]
 struct RedisCommand {
@@ -14,28 +31,119 @@ struct RedisCommand {
     str_cmd: Vec<String>,  // Command arguments (e.g., ["ECHO", "hey"])
 }
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+impl RedisCommand {
+    fn parser_receive(command_str: &str) -> Result<Self, String> {
+        let mut lines = command_str.split("\r\n").filter(|x| !x.is_empty());
+        let mut cmd_len = 0;
+        let mut n_chars_len = Vec::new();
+        let mut str_cmd = Vec::new();
 
-    // Uncomment this block to pass the first stage
-    //
+        while let Some(line) = lines.next() {
+            match line.chars().next() {
+                Some('*') => {
+                    cmd_len = line
+                        .trim_start_matches('*')
+                        .parse::<i32>()
+                        .expect("Failed to parse command length");
+                }
+
+                Some('$') => {
+                    n_chars_len.push(
+                        line.trim_start_matches('$')
+                            .parse::<i32>()
+                            .expect("Failed to parse character length"),
+                    );
+                }
+
+                _ => str_cmd.push(line.to_lowercase()),
+            }
+        }
+
+        Ok(Self {
+            cmd_len,
+            n_chars_len,
+            str_cmd,
+        })
+    }
+
+    fn format_response_code(&self, input: Option<String>) -> String {
+        let chars_len: Vec<i32>;
+        let str_cmd_to_use = match input {
+            Some(s) => {
+                chars_len = vec![0, s.len() as i32];
+                vec![s]
+            }
+            None => {
+                chars_len = self.n_chars_len.clone();
+                self.str_cmd[1..].to_vec()
+            }
+        };
+
+        str_cmd_to_use
+            .iter()
+            .enumerate()
+            .map(|(i, item)| format!("${}\r\n{}\r\n", chars_len[i + 1], item))
+            .collect()
+    }
+}
+
+struct RedisData {
+    data: HashMap<String, String>,
+}
+
+impl RedisData {
+    fn init_db() -> Self {
+        Self {
+            data: HashMap::new(),
+        }
+    }
+
+    fn set_value(&mut self, command: &RedisCommand) -> RedisResponse {
+        self.data
+            .insert(command.str_cmd[1].clone(), command.str_cmd[2].clone());
+        RedisResponse::OK(String::from("OK"))
+    }
+
+    fn get_value(&self, command: &RedisCommand) -> Option<String> {
+        let key = &command.str_cmd[1];
+        match self.data.get(key) {
+            Some(value) => Some(value.clone()),
+            None => None,
+        }
+    }
+}
+
+fn main() {
+    println!("Logs from your program will appear here!");
+    let mut handles = vec![];
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    let redis_data = RedisData::init_db();
+    let db_instances = Arc::new(RwLock::new(redis_data));
+
     for stream in listener.incoming() {
-        thread::spawn(move || match stream {
+        let db_instances = Arc::clone(&db_instances);
+        let handle = thread::spawn(move || match stream {
             Ok(mut stream) => {
                 println!("connected");
-                event_handler(&mut stream);
+                //let mut redis_db = db_instances.lock().unwrap();
+                event_handler(&mut stream, db_instances);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
 
-fn event_handler(stream: &mut TcpStream) {
+fn event_handler(stream: &mut TcpStream, db_instances: Arc<RwLock<RedisData>>) {
+    println!("event connected");
     let mut buf = [0; 1024];
     loop {
         let reader = stream.read(&mut buf).unwrap();
@@ -46,33 +154,51 @@ fn event_handler(stream: &mut TcpStream) {
         }
         .trim_end_matches('\0');
 
-        let command = parser_receive(&readable);
+        let command = RedisCommand::parser_receive(&readable)
+            .expect("Error when trying to parse the command");
 
-        let first_cmd = &command.str_cmd[0];
+        if let Some(first_cmd) = command.str_cmd.get(0) {
+            match first_cmd.as_str() {
+                "echo" => {
+                    let final_str = command.format_response_code(None);
+                    stream
+                        .write_all(final_str.as_bytes())
+                        .expect("failed to write to client");
+                }
 
-        //let lst_command = &command.str_cmd;
-        //
-        //for (i, com) in lst_command.iter().enumerate() {
-        //    if i == 0{
-        //
-        //    }
-        //}
+                "ping" => {
+                    stream
+                        .write_all(b"+PONG\r\n")
+                        .expect("failed to write to client");
+                }
 
-        //println!("{:?}", command);
-        match first_cmd.as_str() {
-            "echo" => {
-                let tmp_str = &command.str_cmd[1..];
-                let n_len = &command.n_chars_len[1..];
-                let final_str = parser_send(tmp_str, n_len);
-                //println!("{:?}", final_str);
-                stream.write_all(final_str.as_bytes()).unwrap();
+                "set" => {
+                    let mut db = db_instances.write().unwrap();
+                    let response = db.set_value(&command);
+                    //println!("{:?}", &db.data);
+                    stream
+                        .write_all(response.to_string().as_bytes())
+                        .expect("failed to write to client");
+                }
+
+                "get" => {
+                    let mut db = db_instances.read().unwrap();
+                    match db.get_value(&command) {
+                        Some(string_return) => {
+                            let parsed_return = command.format_response_code(Some(string_return));
+                            stream
+                                .write_all(parsed_return.as_bytes())
+                                .expect("failed to write to client");
+                        }
+                        None => {
+                            stream
+                                .write_all("$-1\r\n".as_bytes())
+                                .expect("failed to write to client");
+                        }
+                    };
+                }
+                _ => {}
             }
-
-            "ping" => {
-                stream.write_all(b"+PONG\r\n").unwrap();
-            }
-
-            _ => {}
         }
 
         if reader == 0 {
@@ -80,69 +206,3 @@ fn event_handler(stream: &mut TcpStream) {
         }
     }
 }
-
-fn parser_send(str_cmd: &[String], n_chars_len: &[i32]) -> String {
-    let mut final_str = String::new();
-    for (i, item) in str_cmd.iter().enumerate() {
-        final_str.push('$');
-        final_str.push_str((n_chars_len[i]).to_string().as_str());
-        final_str.push_str("\r\n");
-        final_str.push_str(item);
-        final_str.push_str("\r\n");
-    }
-    return final_str;
-}
-
-fn parser_receive(command_str: &str) -> RedisCommand {
-    let mut lines = command_str.split("\r\n").filter(|x| !x.is_empty());
-    let mut cmd_len = 0;
-    let mut n_chars_len = Vec::new();
-    let mut str_cmd = Vec::new();
-
-    while let Some(line) = lines.next() {
-        match line.chars().next() {
-            Some('*') => {
-                cmd_len = line
-                    .trim_start_matches('*')
-                    .parse::<i32>()
-                    .expect("Failed to parse command length");
-            }
-
-            Some('$') => {
-                n_chars_len.push(
-                    line.trim_start_matches('$')
-                        .parse::<i32>()
-                        .expect("Failed to parse character length"),
-                );
-            }
-
-            _ => str_cmd.push(line.to_lowercase()),
-        }
-    }
-
-    RedisCommand {
-        cmd_len,
-        n_chars_len,
-        str_cmd,
-    }
-}
-
-//fn parser(buf: &[u8]) {
-//    if buf.is_empty() {
-//        return;
-//    }
-//
-//    let mut num: u8;
-//    let mut counter = 0;
-//
-//    loop {
-//        num = buf[counter];
-//        if num == 0 {
-//            break;
-//        }
-//        println!("{}", &num);
-//
-//
-//        counter += 1;
-//    }
-//}
